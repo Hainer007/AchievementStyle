abb5ed204cbfd47a8af06698fd86c6217d3c54f7
Hainer007 <hainertv@gmail.com>
2025-10-03 09:36:43 +0300

    Changelog: - Added support for Mod Menu - Updated compatibility to Minecraft 1.21.9 and 1.21.10-rc1
    If you have any ideas or suggestions, feel free to share them in the GitHub issues.

--- src/main/java/hainer/mod/achievementstyle/integration/ModMenuIntegration.java
+++ src/main/java/hainer/mod/achievementstyle/integration/ModMenuIntegration.java
@@ -1,13 +1,13 @@
-//package hainer.mod.achievementstyle.integration;
-//
-//import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-//import com.terraformersmc.modmenu.api.ModMenuApi;
-//import hainer.mod.achievementstyle.config.AchievementConfig;
-//
-//public class ModMenuIntegration implements ModMenuApi {
-//
-//    @Override
-//    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-//        return AchievementConfig::createConfigScreen;
-//    }
-//}
+package hainer.mod.achievementstyle.integration;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import hainer.mod.achievementstyle.config.AchievementConfig;
+
+public class ModMenuIntegration implements ModMenuApi {
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return AchievementConfig::createConfigScreen;
+    }
+}
